cc7e77bdf5cd5d75154c635203d8e21fad1c3baf
Pâmela Amorim <pamelaamorim8@hotmail.com>
2023-02-16 11:38:59 -0300

    incluindo pastas.

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') { // Tanto faz == ou !=
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
