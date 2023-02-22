c05ca37ae838f53e9c0d0b39c33f41ecb5550f02
Pâmela Amorim <pamelaamorim8@hotmail.com>
2023-02-22 18:56:30 -0300

    aula de repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+/*var c = 1 
+do {
+    console.log(`Número --> ${c}`)  maneira correta 
+    c++
+} while (c <=10) */
+
+
+
+var c = 1 
+while (c <= 6) {
+    console.log(`Passo ${c}`) /*maneira correta e mais encontrada nos programas */
+    c++
+} 
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')  pior maneira de fazer
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
